d9e8850231a121b72479fdb51c0ee260ad4cab0c
dimash <200103433@stu.sdu.edu.kz>
2023-01-31 23:49:02 +0600

    Main Page View

---
+++ RSP/MainViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  RSP
+//
+//  Created by Dinmukhamed on 31.01.2023.
+//
+
+import UIKit
+import SnapKit
+
+class MainViewController: UIViewController {
+    let backgroundImageView = UIImageView()
+    let singlePlayerButton = UIButton()
+    let multiPlayerButton = UIButton()
+    let welcomeLabel = UILabel()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialize()
+    }
+
+
+}
+
+extension MainViewController{
+    private func initialize(){
+        
+        view.addSubview(backgroundImageView)
+        backgroundImageView.image = UIImage(named: "BackgroundImage")
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+       
+        view.addSubview(welcomeLabel)
+        welcomeLabel.text = "Welcome to the game"
+        welcomeLabel.textColor = .white
+        welcomeLabel.numberOfLines = 0
+        welcomeLabel.lineBreakMode = .byWordWrapping
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.font = UIFont.systemFont(ofSize: 54.0)
+        welcomeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(92)
+            make.right.left.equalToSuperview().inset(16)
+        }
+        
+        view.addSubview(singlePlayerButton)
+        singlePlayerButton.layer.backgroundColor = UIColor(red: 0.404, green: 0.314, blue: 0.643, alpha: 1).cgColor
+        singlePlayerButton.layer.cornerRadius = 8
+        singlePlayerButton.setTitle("Single Player", for: .normal)
+        singlePlayerButton.setTitleColor(.white, for: .normal)
+        singlePlayerButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(666)
+            make.height.equalTo(50)
+            make.left.right.equalToSuperview().inset(16)
+        }
+        view.addSubview(multiPlayerButton)
+        multiPlayerButton.layer.backgroundColor = UIColor(red: 0.404, green: 0.314, blue: 0.643, alpha: 1).cgColor
+        multiPlayerButton.layer.cornerRadius = 8
+        multiPlayerButton.setTitle("Multi Player", for: .normal)
+        multiPlayerButton.setTitleColor(.white, for: .normal)
+        multiPlayerButton.snp.makeConstraints { make in
+            make.top.equalTo(singlePlayerButton.snp.bottom).offset(10)
+            make.height.equalTo(50)
+            make.left.right.equalToSuperview().inset(16)
+        }
+    }
+    
+}
+
